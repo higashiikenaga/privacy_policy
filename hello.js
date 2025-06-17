f7bbdffaae006e040936bdc0b2aa85acd9ad340c
higashiikenaga <higashiikenaga@gmail.com>
2025-06-17 12:33:12 +0900

    Delete hello.js

--- hello.js
+++
@@ -1,5 +0,0 @@
-// functions/hello.js
-export async function onRequest(context) {
-  console.log("[HelloFunction] Request received! Hello, world!");
-  return new Response("Hello from Cloudflare Functions!", { status: 200 });
-}
